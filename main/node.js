1c37ac886347ca0e33642205a5a81833dbc0e597
Sammy Burns <sburns2023@kehillahstudent.org>
2023-04-30 22:24:02 -0700

    ui changes

--- main/node.js
+++ main/node.js
@@ -5,12 +5,20 @@ app.get("/", function (req, res) {
   res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/main.html");
 });
 
+app.get("/resume", function (req, res) {
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/resume.html");
+});
+
 app.get("/sampic", function (req, res) {
   res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/sam.png");
 });
 
 app.get("/insta", function (req, res) {
   res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/insta.png");
+});
+
+app.get("/email", function (req, res) {
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/email.png");
 });
 
 app.listen(3000);
