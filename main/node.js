2192eb0ba52e5c4c4e28e6069782fe3bdd9e5e69
Sammy Burns <sburns2023@kehillahstudent.org>
2023-05-03 17:01:33 -0700

    added login page, new user page, working database

--- main/node.js
+++ main/node.js
@@ -1,7 +1,65 @@
 const http = require("http");
 var express = require("express");
 var app = express();
+const fs = require("fs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/createnewuser", (req, res) => {
+  const user = req.body.user;
+  const pass = req.body.pass;
+  console.log(user + "-" + pass + "\n");
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/error.html");
+  console.log("i ran");
+  fs.appendFile(
+    "/Users/samuelburns/Desktop/samWebpage/main/database.txt",
+    user + "-" + pass + "\n",
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  console.log("i ran again");
+});
+
+app.post("/login", (req, res) => {
+  const user = req.body.user;
+  const pass = req.body.pass;
+  var text = "";
+  var lines = "";
+  data = fs.readFileSync(
+    "/Users/samuelburns/Desktop/samWebpage/main/database.txt",
+    "utf8",
+    (err) => {
+      if (err) throw err;
+    }
+  );
+
+  text = data.toString();
+  lines = text.split("\n");
+
+  var access = false;
+  for (var i = 0; i < lines.length; i++) {
+    const info = lines[i].split("-");
+    if (user == info[0] && pass == info[1]) {
+      res.redirect("/main");
+      console.log("entry granted");
+      access = true;
+    }
+  }
+  if (!access)
+    res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/error.html");
+
+  // console.log(
+  //   `Received form submission with user=${user} and password=${pass}`
+  // );
+});
+
 app.get("/", function (req, res) {
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/login.html");
+});
+
+app.get("/main", function (req, res) {
   res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/main.html");
 });
 
@@ -28,4 +86,22 @@ app.get("/resume.css", function (req, res) {
 app.get("/main.css", function (req, res) {
   res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/main.css");
 });
+
+app.get("/login.css", function (req, res) {
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/login.css");
+});
+
+app.get("/loginerror", function (req, res) {
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/error.html");
+});
+
+app.get("/newuser", function (req, res) {
+  res.sendFile("/Users/samuelburns/Desktop/samWebpage/main/newuser.html");
+});
+// app.post('/login', function (req, res) {
+//   // if (req.body.username == 'sam' && req.body.password == 'sam')
+//   //   res.sendFile('/Users/samuelburns/Desktop/samWebpage/main/main.html');
+//   // else res.status(401).send('Wrong username or password');
+// });
+
 app.listen(3000);
